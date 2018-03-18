3fceda58845e3bc4e7ceac0dc1f48652e78a3d2d
Karakol <Karakol@10.1.10.73>
2018-03-18 17:33:17 -0500

    #3

---
+++ src/test/java/tests/NewTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
