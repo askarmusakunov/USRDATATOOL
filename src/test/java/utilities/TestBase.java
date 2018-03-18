b47b7f80efaa1dd42753d77eba8ed2dab6797c0c
Karakol <Karakol@10.1.10.73>
2018-03-18 17:31:13 -0500

    #2

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,5 +1,27 @@
 package utilities;
 
-public class TestBase {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import static utilities.Driver.getDriver;
+public class TestBase {
+	
+;
+	
+	
+	@BeforeClass(alwaysRun=true)
+	public void setUP() {
+						
+		getDriver();
+		getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		getDriver().manage().window().maximize();		
+		getDriver().get(Config.getProperty("url"));
+	}
+	
+	//@AfterClass(alwaysRun=true)
+	public void tearDown() {
+		Driver.closeDriver();
+	}
+	
 }
